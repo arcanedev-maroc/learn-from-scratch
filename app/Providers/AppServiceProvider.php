bd50aa1ebb667923bfb1ff02350f3dbde61b2f27
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-23 17:29:21 +0000

    Cleaning/Preparing the project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,40 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register any application services.
+     */
+    public function register()
+    {
+        if ($this->isLocalEnvironment()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
+    }
+
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
         //
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
      */
-    public function register()
+    /**
+     * Check the environment is local.
+     *
+     * @return bool
+     */
+    private function isLocalEnvironment()
     {
-        //
+        return $this->app->environment() === 'local';
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,15 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+namespace App\Tests;
+
+use Illuminate\Foundation\Testing\TestCase as IlluminateTestCase;
+
+class TestCase extends IlluminateTestCase
 {
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
     /**
      * The base URL to use while testing the application.
      *
@@ -9,6 +17,10 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     protected $baseUrl = 'http://localhost';
 
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
     /**
      * Creates the application.
      *
@@ -18,7 +30,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
     }
