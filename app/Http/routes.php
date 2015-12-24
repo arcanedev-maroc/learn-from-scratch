bf27f50081d84ff6613ce5bb5746f91673268bb6
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-24 18:12:17 +0000

    Adding basic routes for topics

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,35 +1,96 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/feedback', function () {
-    return "You have been clicked.";
-});
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
+/* ------------------------------------------------------------------------------------------------
+ |  Application Routes
+ | ------------------------------------------------------------------------------------------------
+ | !!! IMPORTANT !!!
+ |
+ |   NEVER USE `Route::controller()` OR `Route::resource()`.
+ |
+ |   This is what happen if you used them : (ノ ゜Д゜)ノ ︵ ┻━┻
+ */
 Route::group(['middleware' => ['web']], function () {
-    //
+    /* ------------------------------------------------------------------------------------------------
+     |  Frontend Routes
+     | ------------------------------------------------------------------------------------------------
+     */
+    Route::group([
+        'namespace' => 'Frontend',
+    ], function () {
+
+        /**
+         * Pages Routes
+         */
+        Route::get('/', [
+            'as'   => 'public::home',
+            'uses' => 'PagesController@index',
+        ]);
+
+        /**
+         * Topics Routes
+         */
+        Route::group([
+            'prefix' => 'topics',
+        ], function () {
+            Route::get('/', [
+                'as'   => 'public::topics.index',
+                'uses' => 'TopicsController@index',
+            ]);
+
+            Route::get('{topic}', [
+                'as'   => 'public::topics.show',
+                'uses' => 'TopicsController@show',
+            ]);
+        });
+    });
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Backend Routes
+     | ------------------------------------------------------------------------------------------------
+     */
+    Route::group([
+        'namespace' => 'Backend',
+    ], function () {
+        /**
+         * Topics Routes
+         */
+        Route::group([
+            'prefix' => 'topics',
+        ], function () {
+            Route::get('/', [
+                'as'   => 'public::topics.index',
+                'uses' => 'TopicsController@index',
+            ]);
+
+            Route::get('{topic}', [
+                'as'   => 'public::topics.show',
+                'uses' => 'TopicsController@show',
+            ]);
+
+            Route::get('create', [
+                'as'   => 'public::topics.create',
+                'uses' => 'TopicsController@create',
+            ]);
+
+            Route::post('store', [
+                'as'   => 'public::topics.store',
+                'uses' => 'TopicsController@store',
+            ]);
+
+            Route::get('{topic}/edit', [
+                'as'   => 'public::topics.edit',
+                'uses' => 'TopicsController@edit',
+            ]);
+
+            Route::put('{topic}/update', [
+                'as'   => 'public::topics.store',
+                'uses' => 'TopicsController@store',
+            ]);
+
+            Route::delete('{topic}', [
+                'as'   => 'public::topics.delete',
+                'uses' => 'TopicsController@delete',
+            ]);
+        });
+    });
 });
